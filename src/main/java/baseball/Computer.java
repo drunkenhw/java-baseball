c4740cabe5233f881fc4024779caea8c697355b9
drunkenhw <drunkenhw@gmail.com>
2022-11-03 21:00:11 +0900

    feat : 컴퓨터가 임의의 서로 다른 3개의 숫자를 생성한다.

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Computer {
+
+    private final List<Integer> answer;
+
+    public Computer() {
+        this.answer = generateAnswer();
+    }
+
+    public List<Integer> generateAnswer() {
+        Set<Integer> uniqueAnswer = new HashSet<>();
+        while (uniqueAnswer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            uniqueAnswer.add(randomNumber);
+        }
+        return new ArrayList<>(uniqueAnswer);
+    }
+}
