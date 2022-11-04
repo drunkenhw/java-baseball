4b8c43b85f1ef0c4a8cdc3fc64be9393f85dea55
drunkenhw <drunkenhw@gmail.com>
2022-11-04 19:34:32 +0900

    feat(InputView) : 플레이어에게 서로 다른 숫자를 입력 받는다

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,9 @@
 package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -12,10 +15,37 @@ public class InputView {
         return number;
     }
 
+    public static List<Integer> getUniqueDigits(int count) {
+        String input = inputNumber();
+        List<Integer> digits = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        validateUniqueNumber(digits);
+        validateCount(digits, count);
+
+        return digits;
+    }
+
     private static void validateNumber(String input) {
         if (!input.matches(NUMBER_PATTERN)) {
             throw new IllegalArgumentException("숫자가 아닙니다.");
         }
     }
 
+    private static void validateUniqueNumber(List<Integer> numbers) {
+        int uniqueNumberSize = (int) numbers.stream()
+                .distinct()
+                .count();
+
+        if (numbers.size() != uniqueNumberSize) {
+            throw new IllegalArgumentException("서로 다른 숫자가 아닙니다");
+        }
+    }
+
+    private static void validateCount(List<Integer> numbers, int count) {
+        if (numbers.size() != count) {
+            throw new IllegalArgumentException("입력하신 숫자는 " + numbers.size() + "개 입니다. " + count + "개를 입력해주세요");
+        }
+    }
 }
